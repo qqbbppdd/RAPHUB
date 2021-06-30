bcfede73eb5f5001a0d365f091fa97b8e9020269
qqbbppdd <76839089+qqbbppdd@users.noreply.github.com>
2021-06-30 15:44:25 +0300

    Add files via upload

--- js/img-resizing.js
+++ js/img-resizing.js
@@ -67,56 +67,6 @@ const FitObjects = [];
 for(var a = 0; a < FitObjectsRaw.length;a+=1)
     FitObjects.push(new FitObject(a));
 
-/*
-for(var a = 0; a < FitObjectsRaw.length;a+=1) {
-    FitObjectsRaw[a].onload = function() {
-        FitObjects.push(FitObjectsRaw[a]);
-        alert(a);
-        function Appear(id) {
-            setTimeout(function(){FitObjects[id].style.animation = "appear .4s ease-in-out forwards"},id*35 )
-        }
-        Appear(a);
-        var res = FitObjects[a].getAttribute('res');
-        var persp = FitObjects[a].getAttribute('persp');
-    
-        if(persp) FitObjects[a].style.objectPosition = persp;
-    
-        var raw = res.split('x');
-        var width = raw[0] == '_'? NaN : parseInt(raw[0]);
-        var height = raw[1] == '_'? NaN : parseInt(raw[1]);
-    
-        if(!width && !height) {
-            width = FitObjects[a].naturalWidth;
-            height = FitObjects[a].naturalHeight;
-        }
-        else if(!width && height)
-            width = FitObjects[a].naturalWidth * (height / FitObjects[a].naturalHeight);
-        else if(width && !height)
-            height = FitObjects[a].naturalHeight * (width / FitObjects[a].naturalWidth);
-        
-        FitObjects[a].style.width = width + "px";
-        FitObjects[a].style.height = height + "px";
-        FitObjects[a].style.minHeight = Math.ceil(height * 0.75) + "px";
-        InitialSizes.push({"width":width,"height":height});
-    }
-}
-
-    for(var a = 0; a < FitObjects.length;a+=1) {
-        var parentWidth = FitObjects[a].parentElement.clientWidth;
-        var width = FitObjects[a].clientWidth;
-        var height = FitObjects[a].clientHeight;
-        if(parentWidth <= InitialSizes[a].width || width / parentWidth >= 0.5) {
-            FitObjects[a].style.width = parentWidth + 'px';
-            FitObjects[a].style.height = InitialSizes[a].height *  parentWidth / InitialSizes[a].width + 'px';
-
-        }
-        if(document.body.clientWidth > 600 && FitObjects[a].clientWidth > InitialSizes[a].width) {
-            FitObjects[a].style.width = InitialSizes[a].width + 'px';
-            FitObjects[a].style.height = InitialSizes[a].height + 'px';
-        }
-    }
-*/
-
 
 function Reset() {
     for(var a = 0; a < FitObjects.length;a+=1)
--- js/music.js
+++ js/music.js
@@ -1,12 +1,51 @@
 const holder = document.querySelector(".tracks");
+const queue = [];
+let progress = 0;
 
-for(var a = 0; a < music.length;a+=1) {
+holder.classList.add("loading");
+
+for(var a = 0; a < music.length;a+=1) 
     holder.innerHTML += '<div class="qblock trackholder"><h1>' +music[a].name +'</h1><audio controls><source src="' +music[a].path +'" type="audio/mpeg"></audio><div class="buttonholder"><a class="qbutton to-clip" target="_blank" href="' + music[a].video+'">Клип</a><a class="qbutton download" href="' +music[a].path +'" download>Скачать</a></div></div>';
+
+let audios = document.querySelectorAll("audio");
+
+function Activate(id) {
+    setTimeout(function() {
+        if(id == 0) holder.classList.remove("loading");
+        audios[id].parentElement.style.visibility = "visible";
+        audios[id].parentElement.style.animation = "appear .4s forwards ease-in-out";
+
+        //Когда последний плеер загружен,
+        //Подвал сайта становится видимым.
+        if(id == audios.length - 1)
+            document.querySelector("footer").style.visibility = "visible";
+    },30*id);
 }
 
-//Когда последний плеер добавлен на страницу,
-//Подвал сайта становится видимым.
-document.querySelector("footer").style.visibility = "visible";
+function Queue() {
+    if(queue.length == 0) return;
+    var index = queue.indexOf(progress);
+    if(index == -1) return;
+    queue.splice(index,1);
+    Activate(progress);
+    progress+=1;
+    Queue();
+}
+
+function Configure(id) {
+    audios[id].oncanplay = function() {
+        if(progress == id) {
+            Activate(id);
+            progress+=1;
+            Queue();
+            return;
+        }
+        queue.push(id);
+    };
+}
+
+for(var a = 0; a < audios.length;a+=1)
+    Configure(a);
 
 const players = document.querySelectorAll("audio");
 
